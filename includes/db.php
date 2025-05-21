4033b359038936e0efa0fd10d31ed863c59a8230
umarzamann <beingumar9@gmail.com>
2025-05-21 16:21:42 +0500

    updating db name

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "posAli";
+$dbname = "pos";
 
 $connection = mysqli_connect ($servername, $username, $password, $dbname);
 
